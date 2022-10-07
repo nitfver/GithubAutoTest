00f01f00c706fb2a56858562cfae55b748ecff22
nitfver <nitfver@gmail.com>
2022-10-07 12:25:25 +0530

    Round 3 of doing the changes in Login.java

--- ATestGit/src/anc/Login.java
+++ ATestGit/src/anc/Login.java
@@ -65,7 +65,7 @@ public class Login {
 		Thread.sleep(3000); 
 		 
 				
-		System.out.println("WE ARE LOGGED IN INTO the system");
+		System.out.println("WE ARE LOGGED IN INTO THE SYSTEM");
 		//driver.close();
 		
 	}
